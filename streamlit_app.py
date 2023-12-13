d9694cdc5718032147b2e7ef81f9728ee91b1567
Ig0r_ML <117780098+Zagalskiy@users.noreply.github.com>
2023-12-13 16:47:36 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,22 @@
-import streamlit as st  # - фреймворк для развертывания моделей и визуализаций
-
+import streamlit as st
+import pandas as pd
 
 # Задание широкоформатного режима страницы и указание заголовка
 st.set_page_config(layout="wide", page_title="UniGoTo", page_icon=":ai:")
 
-with row1_1:
-    st.title("UniGoTo")
+st.title("UniGoTo")
+st.write("Рекомендательный сервис ВУЗов и специальностей на основе технологии Искусственного Интеллекта.")
 
-with row1_2:
-    st.write(
-        """
-    ##
-    Рекомендательный сервис ВУЗов и специальностей на основе технологии Искусственного Интеллекта.
-    """
-    )
+interests = ''
+
+with st.form("interests_form"):
+        st.write("Введите ваши данные и предпочтения (на русском языке, можно без точек и запятых).")
+        interest1 = st.text_input('О себе', '')
+        interest2 = st.text_input('Сфера деятельности', '')
+        interest3 = st.text_input('Любимые книги', '')
+        interest4 = st.text_input('Любимые игры', '')
+        interest5 = st.text_input('Интересы и хобби', '')
+        submitted = st.form_submit_button("Отправить")
+        
+        if submitted:
+                interests = st.write(interest1 + ' ' + interest2 + ' ' + interest3 + ' ' + interest4 + ' ' + interest5)
